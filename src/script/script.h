b77c3549399cbde978ded3a9177537e0b8e6079f
f3l <coldcity@gmail.com>
2018-06-21 17:19:48 +0100

    OP_BEE added

--- src/script/script.h
+++ src/script/script.h
@@ -181,6 +181,8 @@ enum opcodetype
     OP_NOP9 = 0xb8,
     OP_NOP10 = 0xb9,
 
+    // LitecoinCash: HIVEMINE: Bee opcode
+    OP_BEE = 0xbe,
 
     // template matching params
     OP_SMALLINTEGER = 0xfa,
