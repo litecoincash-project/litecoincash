2deb21397235afc33080a273848ee35926b7f6b8
Loxley <loxley@litecoinca.sh>
2018-07-17 10:42:54 +0100

    Fix URI parseing with 'litecoincash://' as in issue #13

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -212,7 +212,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //    which will lower-case it (and thus invalidate the address).
     if(uri.startsWith("litecoincash://", Qt::CaseInsensitive))
     {
-        uri.replace(0, 11, "litecoincash:");
+        uri.replace(0, 15, "litecoincash:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
