adca1416dcbe436eeea0cbae1c397a688ebc394d
f3l <coldcity@gmail.com>
2018-09-21 16:10:33 +0100

    Fix CVE-2018-17144

--- src/validation.cpp
+++ src/validation.cpp
@@ -3030,7 +3030,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 
     // Check transactions
     for (const auto& tx : block.vtx)
-        if (!CheckTransaction(*tx, state, false))
+        if (!CheckTransaction(*tx, state, true))    // LitecoinCash: Fix CVE-2018-17144
             return state.Invalid(false, state.GetRejectCode(), state.GetRejectReason(),
                                  strprintf("Transaction check failed (tx hash %s) %s", tx->GetHash().ToString(), state.GetDebugMessage()));
 
